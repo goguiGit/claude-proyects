260fec9acf4ffccb5be795a0d8e2b4962b612645
Rodrigo Valdez <rodrigo.valdez@visma.com>
2026-05-18 14:59:52 +0200

    feat: add request logger middleware

--- issue_3/proyect_1/src/app.ts
+++ issue_3/proyect_1/src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import healthRouter from './health/health.router.js';
+import { requestLogger } from './middleware/logger.js';
 
 export function createApp() {
   const app = express();
+  app.use(requestLogger);
   app.use(express.json());
   app.use('/health', healthRouter);
   return app;
